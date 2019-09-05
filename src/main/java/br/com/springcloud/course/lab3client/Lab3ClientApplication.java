79af8669acdc425aa2fe9d3f00b58b14b850e99e
Marcos Leandro Francischinelli <leandro.francischinelli@gmail.com>
2019-09-04 22:10:03 -0300

    lab 3 client

---
+++ src/main/java/br/com/springcloud/course/lab3client/Lab3ClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.springcloud.course.lab3client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ClientApplication.class, args);
+	}
+
+}
